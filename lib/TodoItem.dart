b2b1fd8597d006f05b68bd24a33cc2785823e3c1
Dong Tan Huy <juliandong@Julians-MacBook-Pro.local>
2019-01-16 17:16:48 +0700

    Add Dialog, Style done tasks, Render empty text

--- lib/TodoItem.dart
+++ lib/TodoItem.dart
@@ -18,31 +18,35 @@ class _TodoItem extends State<TodoItem> {
   _TodoItem(this.text, this.checked);
   void changeChecked(bool value) => setState(() => checked = value);
 
+  TextStyle getTextStyle () => new TextStyle(
+    color: checked ? Colors.red[500] : Colors.black,
+    fontFamily: "Rubik",
+    fontSize: 18.0,
+    decoration: checked ? TextDecoration.lineThrough : TextDecoration.none,
+    decorationColor: Colors.grey[500],
+  );
+
+
   @override
   Widget build(BuildContext context) {
     return new Container (
       padding: const EdgeInsets.only(left: 15, right: 5),
-      margin: const EdgeInsets.only(top:5, bottom:5),
+      margin: const EdgeInsets.only(top:0, bottom:0),
       decoration: new BoxDecoration(
-        border: new Border(top: BorderSide.none, left: BorderSide.none, right: BorderSide.none, bottom: BorderSide(width:1, style: BorderStyle.solid)),
+        border: new Border(top: BorderSide.none, left: BorderSide.none, right: BorderSide.none, bottom: BorderSide(color: Colors.grey[500] ,width:1, style: BorderStyle.solid)),
+        color: checked ? Colors.lightGreenAccent : Colors.transparent,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           new Text(
             text,
+            style: getTextStyle(),
           ),
           Container(
               margin: const EdgeInsets.only(left: 40),
               child: Row(
                 children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                        Icons.delete
-                    ),
-                    tooltip: 'Delete todo',
-                    onPressed: () => print('deleted'),
-                  ),
                   Checkbox(
                     value: checked,
                     onChanged: changeChecked,
--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,55 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
-  List<Todo> todos =  [
-    new Todo(text: "Task1"),
-    new Todo(text: "Task2"),
-  ];
+  List<Todo> todos = [];
+  String inputText;
+  void onChangeText(value) => setState(() => inputText = value);
+
+  showAddTodoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          content: new Container(
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Add Task',
+                      hintText: 'Enter task here'
+                    ),
+                    onChanged: onChangeText,
+                  ),
+                )
+              ]
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+            FlatButton(
+              child: const Text('Add task'),
+              onPressed: () {
+                print(inputText);
+                var newTodoObj = new Todo(text: inputText);
+                todos.add(newTodoObj);
+                var updatedTodos = todos;
+                setState(() {
+                  todos = updatedTodos;
+                });
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,16 +91,20 @@ class TodoListState extends State<TodoList> {
         title: Text('Todos'),
       ),
       body: new Center(
-        child: new Column(
+        child: todos.length == 0 ? buildEmpty() : new Column(
           children: buildTodoList(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Increment',
         child: Icon(Icons.add),
-        onPressed: () => print('press'),
+        onPressed: () => showAddTodoDialog(context),
       ),
     );
+  }
+
+  Widget buildEmpty() {
+    return new Text('Empty Task');
   }
 
   List<Widget> buildTodoList() {
